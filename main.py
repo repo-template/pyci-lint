1b7cfef62f8f5375d45478a056b01834a7badb33
nanato12 <admin@okj.info>
2023-03-14 22:17:23 +0900

    Success

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-def add(a, b):
-    return a + b
+from random import choices
+from string import ascii_letters, digits
+
+
+def random_string(s: str, n: int) -> str:
+    return "".join(choices(s, k=n))
+
+
+if __name__ == "__main__":
+    print(random_string(ascii_letters + digits, 10))
